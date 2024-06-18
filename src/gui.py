cbdd4dbe19e25adbe9ad609e7bbc739e0666de1c
北西 <northwest.output.official@gmail.com>
2024-06-18 17:51:52 +0900

    need bug fix

--- src/gui.py
+++ src/gui.py
@@ -1,29 +1,52 @@
-import tkinter as tk
-from tkinter import ttk
-import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
-class Application(tk.Frame):
-    def __init__(self,master):
-        super().__init__(master)
-        self.pack()
+def plot(bin_map: np.ndarray[np.ndarray], width_diff: np.ndarray[float], height_diff: np.ndarray[float], margin: float=0.05):
+    fig = plt.figure("window title", figsize=(8, 8))
+    image_ax = fig.add_axes((0+margin, 0.3+margin, 0.7-margin*2, 0.7-margin*2))
+    image_ax.set_xticks([])
+    image_ax.set_yticks([])
+    plt.set_cmap('binary_r')
 
-        self.master.geometry("300x300")
-        self.master.title("Tkinter with Class Template")
+    width_mean_ax = fig.add_axes([0+margin, 0+margin, 0.7-margin*2, 0.3-margin*2])
+    height_mean_ax = fig.add_axes([0.7+margin, 0.3+margin, 0.3-margin*2, 0.7-margin*2])
 
-        self.create_widgets()
+    width_diff_ax = width_mean_ax.twinx()
+    height_diff_ax = height_mean_ax.twiny()
 
-    def create_widgets(self):
-        pass
+    width_mean_ax.plot(np.arange(bin_map.shape[0]), np.mean(bin_map, axis=0) / 255, color="blue")
+    height_mean_ax.plot(np.mean(bin_map, axis=1) / 255, np.arange(bin_map.shape[1]), color="blue")
 
-    def callBack(self):
-        pass
+    width_diff_ax.plot(np.arange(width_diff.shape[0]), width_diff, color="orange")
+    height_diff_ax.plot(height_diff, np.arange(height_diff.shape[0]), color="orange")
+    
+    width_mean_ax.set_xlim([0, bin_map.shape[0]])
+    height_mean_ax.set_ylim([bin_map.shape[1], 0])
+    width_diff_ax.set_xlim([0, bin_map.shape[0]])
+    height_diff_ax.set_ylim([bin_map.shape[1], 0])
 
-def main():
-    root = tk.Tk()
-    app = Application(master=root)#Inherit
-    app.mainloop()
+    width_mean_ax.yaxis.tick_right()
+    height_mean_ax.xaxis.tick_bottom()
+    width_mean_ax.xaxis.tick_bottom()
+    height_mean_ax.yaxis.tick_right()
 
-if __name__ == "__main__":
-    print(sys.argv)
-    main()
+    width_diff_ax.yaxis.tick_left()
+    height_diff_ax.xaxis.tick_top()
+    # # width_diff_ax.xaxis.tick_top()
+    # # height_diff_ax.yaxis.tick_left()
 
+    width_mean_ax.grid()
+    height_mean_ax.grid()
+
+    width_mean_ax.set_ylim([0, 1])
+    height_mean_ax.set_xlim([0, 1])
+
+    width_diff_ax.set_ylim([-10, 10])
+    height_diff_ax.set_xlim([-10, 10])
+
+    width_mean_ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    height_mean_ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
+
+    image_ax.imshow(bin_map)
+    plt.show(block=False)
+    input()
--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import sys
 import numpy as np
+from gui import plot
 
 def get_bin_map(path: str) -> np.ndarray[np.ndarray]:
     im = Image.open(path)
+    # im = add_margin(im, 10, 10, 10, 10, 255)
     bin_map = np.asarray(im)
     width = bin_map.shape[0]
     height = bin_map.shape[1]
@@ -12,64 +14,73 @@ def get_bin_map(path: str) -> np.ndarray[np.ndarray]:
     return bin_map, width, height
 
 def rotate_bin_map(bin_map: np.ndarray[np.ndarray], degree: int) -> np.ndarray[np.ndarray]:
+    bin_map.dtype = 'uint8'
     im = Image.fromarray(bin_map)
-    im = im.rotate(degree, fillcolor=(255))
+    im = im.rotate(degree, fillcolor=(255), expand=1)
     bin_map = np.asarray(im)
     return bin_map
 
 def filter_bin_map(bin_map: np.ndarray[np.ndarray], threshold: float=255/2) -> np.ndarray[np.ndarray]:
-    return np.where(bin_map < threshold, 0, 255)
+    filtered_bin_map = [[255 if b > threshold else 0 for b in map] for map in bin_map]
+    return np.asarray(filtered_bin_map)
+    
 
-def check_bin_map(bin_map: np.ndarray[np.ndarray]) -> int:
-    width_max = np.max(np.sum(bin_map, axis=0))
-    height_max = np.max(np.sum(bin_map, axis=1))
-    print(f"{width_max}:{height_max}")
-    return width_max == 255 * bin_map.shape[0] or height_max == 255 * bin_map.shape[1]
+def add_margin(img, top, right, bottom, left, color):
+    width, height = img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    im = Image.new(img.mode, (new_width, new_height), color)
+    im.paste(img, (left, top))
+    return im
+
+# 境目を取得
+def get_border(width_diff: np.ndarray, height_diff: np.ndarray) -> tuple[int]:
+    # print(f"{np.max(width_diff)}:{np.max(width_diff)}")
+    return np.argmax(np.fabs(width_diff)), np.argmax(np.fabs(height_diff))
+
+# 最大の傾きの大きさ取得
+def get_max_value(width_diff: np.ndarray, height_diff: np.ndarray) -> tuple[float]:
+    width_diff_index, height_diff_index = get_border(width_diff, height_diff)
+    width_diff_max = np.fabs(width_diff)[width_diff_index]
+    height_diff_max = np.fabs(height_diff)[height_diff_index]
+    if width_diff_max > height_diff_max:
+        return width_diff_max
+    else:
+        return height_diff_max
+
+def check_border(width_diff: np.ndarray, height_diff: np.ndarray) -> bool:
+    threshold = 5
+    width_diff_max, height_diff_max = get_border(width_diff, height_diff)
+    return width_diff_max > threshold or height_diff_max > threshold
 
 def get_diff_line(bin_map, axis=0) -> np.ndarray:
     mean_line = np.mean(bin_map, axis=axis)
-    print(mean_line.shape)
     diff_line = np.diff(mean_line, n=1)
     return diff_line
 
 if __name__ == "__main__":
-    print(sys.argv)
-    margin = 0.05
-    path = "../images/large/image04.pgm"
+    # ファイルの読み込み
+    path = "../images/large/image01.pgm"
     bin_map, width, height = get_bin_map(path)
-    deg = 35
-    bin_map = rotate_bin_map(bin_map, deg)
-    no_rotated = check_bin_map(bin_map)
-    fig = plt.figure("window title", figsize=(8, 8))
-    image_ax = fig.add_axes((0+margin, 0.3+margin, 0.7-margin*2, 0.7-margin*2))
-    image_ax.set_xticks([])
-    image_ax.set_yticks([])
-    plt.set_cmap('binary_r')
+    bin_map = filter_bin_map(bin_map)
 
-    width_ax = fig.add_axes([0+margin, 0+margin, 0.7-margin*2, 0.3-margin*2])
-    height_ax = fig.add_axes([0.7+margin, 0.3+margin, 0.3-margin*2, 0.7-margin*2])
+    # 回転角度の決定
+    max_diffs = np.empty(360)
+    for deg in range(360):
+        rotated_bin_map = rotate_bin_map(bin_map, deg)
+        print(rotated_bin_map.dtype)
+        width_diff = get_diff_line(rotated_bin_map, axis=0)
+        height_diff = get_diff_line(rotated_bin_map, axis=1)
+        max_diffs[deg] = get_max_value(width_diff, height_diff)
+        # print(max_diffs[deg])
     
-    width_ax.plot(np.arange(width), np.mean(bin_map, axis=0) / 255)
-    height_ax.plot(np.mean(bin_map, axis=1) / 255, np.arange(height))
-    print(get_diff_line(bin_map, axis=0))
-    width_ax.plot(np.arange(width - 1), get_diff_line(bin_map, axis=0))
-    height_ax.plot(get_diff_line(bin_map, axis=1), np.arange(height - 1))
+    correct_deg = np.argmax(max_diffs)
+    print(correct_deg)
+    bin_map = rotate_bin_map(bin_map, correct_deg)
+    width_diff = get_diff_line(bin_map, axis=0)
+    height_diff = get_diff_line(bin_map, axis=1)
 
-    width_ax.set_xlim([0, width])
-    height_ax.set_ylim([0, height])
-    width_ax.set_ylim([-1, 1])
-    height_ax.set_xlim([-1, 1])
+    # plot
+    plot(bin_map, width_diff, height_diff)
 
-    # width_ax.set_xticks([])
-    # height_ax.set_yticks([])
-
-    width_ax.yaxis.tick_right()
-    height_ax.xaxis.tick_bottom()
-    width_ax.xaxis.tick_bottom()
-    height_ax.yaxis.tick_right()
-    width_ax.grid()
-    height_ax.grid()
-
-    image_ax.imshow(bin_map)
-    plt.show(block=False)
-    input()
+   
