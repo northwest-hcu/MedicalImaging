d092bed02f60df88a7a55db1afe2e669549f8760
北西 <northwest.output.official@gmail.com>
2024-07-14 22:13:37 +0900

    upload

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 import numpy as np
+
 from typing import TypeAlias
 
 Bitmap: TypeAlias = np.ndarray[np.ndarray[int]]
+WIDTH_DIRECTION = 1
+HEIGHT_DIRECTION = 0
+
 def im_plot(bitmap: Bitmap, title: str="default") -> plt.Figure:
     fig = plt.figure(title, figsize=(8, 8))
     image_ax = fig.add_axes((0, 0, 1, 1))
@@ -11,7 +16,53 @@ def im_plot(bitmap: Bitmap, title: str="default") -> plt.Figure:
     plt.show(block=False)
     return fig
 
-def plot(bin_map: np.ndarray[np.ndarray], width_diff: np.ndarray[float], height_diff: np.ndarray[float], margin: float=0.05, title: str="window title"):
+def all_plot(
+    bitmap: Bitmap,
+    left_axis_data: np.ndarray[float],
+    right_axis_data: np.ndarray[float],
+    top_axis_data: np.ndarray[float],
+    bottom_axis_data: np.ndarray[float],
+    margin:float=0.05,
+    title: str="window title") -> (plt.Figure, Axes, Axes, Axes, Axes):
+    fig = plt.figure(title, figsize=(8, 8))
+    image_ax = fig.add_axes((0.3+margin, 0.4-margin, 0.3, 0.3))
+    image_ax.set_xticks([])
+    image_ax.set_yticks([])
+    plt.set_cmap('binary_r')
+    left_axis = fig.add_axes((0+margin, 0.4-margin, 0.3, 0.3))
+    right_axis = fig.add_axes((0.6+margin, 0.4-margin, 0.3, 0.3))
+    top_axis = fig.add_axes((0.3+margin, 0.7-margin, 0.3, 0.3))
+    bottom_axis = fig.add_axes((0.3+margin, 0.1-margin, 0.3, 0.3))
+
+    left_axis.plot(left_axis_data, np.arange(bitmap.shape[HEIGHT_DIRECTION]), color='blue')
+    right_axis.plot(right_axis_data, np.arange(bitmap.shape[HEIGHT_DIRECTION]), color='blue')
+    top_axis.plot(np.arange(bitmap.shape[WIDTH_DIRECTION]), top_axis_data, color='blue')
+    bottom_axis.plot(np.arange(bitmap.shape[WIDTH_DIRECTION]), bottom_axis_data, color='blue')
+
+    left_axis.set_xlim([0, bitmap.shape[WIDTH_DIRECTION]])
+    left_axis.set_ylim([bitmap.shape[HEIGHT_DIRECTION], 0])
+    right_axis.set_xlim([0, bitmap.shape[WIDTH_DIRECTION]])
+    right_axis.set_ylim([bitmap.shape[HEIGHT_DIRECTION], 0])
+    top_axis.set_xlim([0, bitmap.shape[WIDTH_DIRECTION]])
+    top_axis.set_ylim([bitmap.shape[HEIGHT_DIRECTION], 0])
+    bottom_axis.set_xlim([0, bitmap.shape[WIDTH_DIRECTION]])
+    bottom_axis.set_ylim([bitmap.shape[HEIGHT_DIRECTION], 0])
+
+    left_axis.set_xticks([])
+    left_axis.set_yticks([])
+    right_axis.set_xticks([])
+    right_axis.set_yticks([])
+    # top_axis.set_xticks([])
+    # top_axis.set_yticks([])
+    # bottom_axis.set_xticks([])
+    # bottom_axis.set_yticks([])
+
+    image_ax.imshow(bitmap)
+    plt.show(block=False)
+    return fig, left_axis, right_axis, top_axis, bottom_axis
+
+
+def plot(bin_map: np.ndarray[np.ndarray], width_axes: np.ndarray[float|int], height_axes: np.ndarray[float|int], margin:float=0.05, title: str="window title"):
     fig = plt.figure(title, figsize=(8, 8))
     image_ax = fig.add_axes((0+margin, 0.3+margin, 0.7-margin*2, 0.7-margin*2))
     image_ax.set_xticks([])
@@ -24,8 +75,8 @@ def plot(bin_map: np.ndarray[np.ndarray], width_diff: np.ndarray[float], height_
     # width_diff_ax = width_mean_ax.twinx()
     # height_diff_ax = height_mean_ax.twiny()
 
-    width_mean_ax.plot(np.arange(bin_map.shape[0]), np.mean(bin_map, axis=0) / 255, color="blue")
-    height_mean_ax.plot(np.mean(bin_map, axis=1) / 255, np.arange(bin_map.shape[1]), color="blue")
+    width_mean_ax.plot(np.arange(bin_map.shape[0]), width_axes, color="blue")
+    height_mean_ax.plot(height_axes, np.arange(bin_map.shape[1]), color="blue")
 
     # width_diff_ax.plot(np.arange(width_diff.shape[0]), width_diff, color="orange")
     # height_diff_ax.plot(height_diff, np.arange(height_diff.shape[0]), color="orange")
@@ -48,14 +99,14 @@ def plot(bin_map: np.ndarray[np.ndarray], width_diff: np.ndarray[float], height_
     width_mean_ax.grid()
     height_mean_ax.grid()
 
-    width_mean_ax.set_ylim([0, 1])
-    height_mean_ax.set_xlim([0, 1])
+    # width_mean_ax.set_ylim([0, 1])
+    # height_mean_ax.set_xlim([0, 1])
 
     # width_diff_ax.set_ylim([-10, 10])
     # height_diff_ax.set_xlim([-10, 10])
 
-    width_mean_ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
-    height_mean_ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
+    # width_mean_ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    # height_mean_ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
 
     image_ax.imshow(bin_map)
     plt.show(block=False)
--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import sys
 import numpy as np
-from gui import plot, im_plot
+from gui import plot, im_plot, all_plot
 from typing import TypeAlias
+from rembg import remove
 
 Bitmap: TypeAlias = np.ndarray[np.ndarray[int]]
 WIDTH_DIRECTION = 1
 HEIGHT_DIRECTION = 0
-
 
 def path2im(path: str) -> Image:
     im = Image.open(path)
@@ -40,55 +40,70 @@ def get_diff_bitmap(bitmap: Bitmap, axis: int = WIDTH_DIRECTION) -> Bitmap:
     new_bitmap = np.diff(bitmap, n=1, axis=axis)
     return new_bitmap
 
+def get_diff_line(line: np.ndarray[int]) -> np.ndarray[float]:
+    new_line = np.diff(line, n=1)
+    return new_line
+
 def filter_bitmap(bitmap: Bitmap, border: int = int(255 / 2)):
     filtered_bitmap = np.where(bitmap < border, 0, 255)
     return filtered_bitmap
 
-def spread_black(bitmap: Bitmap):
-    height, width = bitmap.shape
-    new_bitmap = bitmap.copy()
-    left_index, top_index, right_index, bottom_index = -1, -1, -1, -1
+def get_hit_wave(bitmap: Bitmap, axis: int=WIDTH_DIRECTION) -> np.ndarray[int]:
+    hit = np.any(bitmap == 0, axis=axis)
+    peaks = np.argmax(bitmap == 0, axis=axis)
+    wave = [peak if h else bitmap.shape[axis] - 1 for peak, h in zip(peaks, hit)]
+    return wave
 
-    h = 0
-    # 左端の座標探索 x軸
-    while h < height:
-        left_array = (new_bitmap[h, :] == 255)
-        left_index = np.argmax(left_array)
-        if left_array[left_index]:
-            break
-        h = h + 1
+def get_peak_point(peaks: np.ndarray[float], axis: int=WIDTH_DIRECTION) -> tuple[int]:
+    # hit = np.any(bitmap == 0, axis=axis)
+    # peaks = np.argmax(bitmap == 0, axis=axis)
+    # peaks = [peak if hit else bitmap.shape[axis] for peak, hit in zip(np.argmax(bitmap == 0, axis=axis), np.any(bitmap == 0, axis=axis))]
+    left_side_peaks = peaks.copy()
+    right_side_peaks = peaks.copy()
+    right_side_peaks.reverse()
+    left_side_peak = len(left_side_peaks) - 1
+    right_side_peak = len(right_side_peaks) - 1
+    pre_diff = -float('Inf') 
+    for i in range(left_side_peak):
+        if i + 1 < len(left_side_peaks):
+            diff = (left_side_peaks[i + 1] - left_side_peaks[i])
+            if left_side_peaks[i] > left_side_peaks[i + 1]:
+                left_side_peak = i #left_side_peaks[i]
+                break
+            if diff < pre_diff:
+                left_side_peak = i #left_side_peaks[i]
+                break
+            else:
+                pre_diff = diff
+    pre_diff = -float('Inf') 
+    for i in range(right_side_peak):
+        if i + 1 < len(right_side_peaks):
+            if right_side_peaks[i] > right_side_peaks[i + 1]:
+                right_side_peak = i #right_side_peaks[i]
+                break
+            if diff < pre_diff:
+                right_side_peak = i #right_side_peaks[i]
+                break
+            else:
+                pre_diff = diff
+    right_side_peak = len(peaks) - 1 - right_side_peak
+    print(left_side_peak, right_side_peak)
+    print("peaks left:({0}@{1}), right:({2}@{3})".format(peaks[left_side_peak], left_side_peak, peaks[right_side_peak], right_side_peak))
+    return left_side_peak, right_side_peak
+    
+def area_axes(bitmap: Bitmap) -> tuple[np.ndarray[float]]:
+    left_side = get_hit_wave(bitmap, axis=WIDTH_DIRECTION)
+    right_side = [bitmap.shape[WIDTH_DIRECTION] - wave - 1 for wave in get_hit_wave(
+            np.flip(bitmap, axis=WIDTH_DIRECTION),
+            axis=WIDTH_DIRECTION
+        )]
+    top_side = get_hit_wave(bitmap, axis=HEIGHT_DIRECTION)
+    bottom_side = [bitmap.shape[HEIGHT_DIRECTION] - wave - 1 for wave in get_hit_wave(
+            np.flip(bitmap, axis=HEIGHT_DIRECTION),
+            axis=HEIGHT_DIRECTION
+        )]
+    return left_side, right_side, top_side, bottom_side
 
-    # 上端の頂点座標探索 y軸
-    w = 0
-    while w < width:
-        top_array = (new_bitmap[:, w] == 255)
-        top_index = np.argmax(top_array)
-        if top_array[top_index]:
-            break
-        w = w + 1
-
-    h = height - 1
-    # 右端の頂点座標探索 x軸
-    while h >= 0:
-        right_array = (new_bitmap[h, :] == 255)
-        right_index = np.argmax(right_array)
-        if right_array[right_index]:
-            break
-        h = h - 1
-
-    w = width - 1
-    # 下端の頂点座標探索 x軸
-    while w >= 0:
-        bottom_array = (new_bitmap[:, w] == 255)
-        bottom_index = np.argmax(bottom_array)
-        if bottom_array[bottom_index]:
-            break
-        w = w - 1
-
-    print(top_index, left_index, bottom_index, right_index)
-
-    new_bitmap[top_index:bottom_index, left_index:right_index] = 255
-    return new_bitmap
 
 if __name__ == "__main__":
     # ファイルの読み込み
@@ -99,9 +114,58 @@ if __name__ == "__main__":
         bitmap = im2bitmap(im)
         width_diff_bitmap = get_diff_bitmap(bitmap, axis=WIDTH_DIRECTION)
         height_diff_bitmap = get_diff_bitmap(bitmap, axis=HEIGHT_DIRECTION)
-        diff_bitmap = get_diff_bitmap(width_diff_bitmap, axis=HEIGHT_DIRECTION)
-        filtered_bitmap = filter_bitmap(bitmap)
-        threshold = 254
+        filtered_bitmap = filter_bitmap(bitmap, border=np.mean(np.mean(bitmap)))
+        left_side, right_side, top_side, bottom_side = area_axes(filtered_bitmap)
+
+        all_plot(
+            bitmap,
+            left_side,
+            right_side,
+            top_side,
+            bottom_side,
+            title="area plot"
+        )
+        fig, left_axis, right_axis, top_axis, bottom_axis = all_plot(
+            filtered_bitmap,
+            left_side,
+            right_side,
+            top_side,
+            bottom_side,
+            title="area plot2"
+        )
+        # 左のグラフの修正
+        print("fix left graph")
+        left_side_peak, right_side_peak = get_peak_point([len(left_side) - l - 1 for l in left_side])
+        left_axis.plot(
+            [left_side[left_side_peak], left_side[right_side_peak]],
+            [left_side_peak, right_side_peak],
+            color='orange')
+        left_axis.plot(np.arange(bitmap.shape[WIDTH_DIRECTION]), left_side, color='lime')
+        print("fix right graph")
+        # 右のグラフの修正
+        left_side_peak, right_side_peak = get_peak_point([r for r in right_side])
+        right_axis.plot(
+            [right_side[left_side_peak], right_side[right_side_peak]],
+            [left_side_peak, right_side_peak],
+            color='orange')
+        right_axis.plot(np.arange(bitmap.shape[WIDTH_DIRECTION]), right_side, color='lime')
+        print("fix top graph")
+        # 上グラフの修正
+        left_side_peak, right_side_peak = get_peak_point([len(top_side) - t - 1 for t in top_side])
+        top_axis.plot(
+            [left_side_peak, right_side_peak],
+            [top_side[left_side_peak], top_side[right_side_peak]],
+            color='orange')
+        top_axis.plot(np.arange(bitmap.shape[HEIGHT_DIRECTION]), top_side, color='lime')
+        print("fix bottom graph")
+        # 下グラフの修正
+        left_side_peak, right_side_peak = get_peak_point([b for b in bottom_side])
+        bottom_axis.plot(
+            [left_side_peak, right_side_peak],
+            [bottom_side[left_side_peak], bottom_side[right_side_peak]],
+            color='orange')
+        bottom_axis.plot(np.arange(bitmap.shape[HEIGHT_DIRECTION]), bottom_side, color='lime')
+        plt.show(block=False)
         # for deg in range(50):
         #     rotated_bitmap = im2bitmap(
         #         rotate_img(
@@ -114,14 +178,12 @@ if __name__ == "__main__":
         #     if np.max(width_mean) > threshold or np.max(height_mean) > threshold:
         #         print(f"{deg}° : {width_mean} : {height_mean} ")
         #         plot(rotated_bitmap, width_mean, height_mean, title=path + f"[rotate: {deg}°]")
-        sb = spread_black(filtered_bitmap)
-        im_plot(bitmap, title=path)
-        im_plot(width_diff_bitmap, title=path + "[width diff]")
-        im_plot(height_diff_bitmap, title=path + "[height diff]")
-        im_plot(filtered_bitmap, title=path + "[filtered]")
-        im_plot(sb, title=path + "[spread black]")
-        plot(bitmap, np.mean(width_diff_bitmap, axis=WIDTH_DIRECTION), np.mean(height_diff_bitmap, axis=HEIGHT_DIRECTION))
-        plot(filtered_bitmap, np.mean(filtered_bitmap, axis=WIDTH_DIRECTION), np.mean(filtered_bitmap, axis=HEIGHT_DIRECTION))
+        # plot(bitmap, np.mean(bitmap, axis=WIDTH_DIRECTION), np.mean(bitmap, axis=HEIGHT_DIRECTION), title="bitmap {0}".format(path))
+        # plot(bitmap, get_hit_wave(bitmap, axis=HEIGHT_DIRECTION), get_hit_wave(bitmap, axis=WIDTH_DIRECTION), title="bitmap hitwave {0}".format(path))
+        # plot(filtered_bitmap, np.mean(filtered_bitmap, axis=WIDTH_DIRECTION), np.mean(filtered_bitmap, axis=HEIGHT_DIRECTION), title="filtered {0}".format(path))
+        # plot(filtered_bitmap, get_hit_wave(filtered_bitmap, axis=HEIGHT_DIRECTION), get_hit_wave(filtered_bitmap, axis=WIDTH_DIRECTION), title="filtered hit wave{0}".format(path))
+        # plot(filtered_bitmap, get_hit_wave(np.flip(filtered_bitmap, axis=HEIGHT_DIRECTION), axis=HEIGHT_DIRECTION), get_hit_wave(np.flip(filtered_bitmap, axis=WIDTH_DIRECTION), axis=WIDTH_DIRECTION), title="reversed hit wave{0}".format(path))
+        # plot(filtered_bitmap, np.mean(filtered_bitmap, axis=WIDTH_DIRECTION), np.mean(filtered_bitmap, axis=HEIGHT_DIRECTION))
         break
     input()
 
